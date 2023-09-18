6e86080ed3d7592cde5fe5e081bfd540be422d15
sanjayjc97 <sanjayjc97@gmail.com>
2023-09-18 16:08:25 +0530

    added logger to views

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,11 @@ from django.views import View
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 #local imports 
 
 from .models import (
@@ -32,6 +37,7 @@ class List_todo(APIView):
             else:
                 return Response({"status":0,'message':"No todos found for this user","data":None},status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
+            logger.error(f"Error occured in list-todo: {str(e)}")
             return Response({
                 'status':0,
                 "message":str(e),
@@ -61,6 +67,7 @@ class Create_todo(APIView):
                     "data":None
                 },status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
+            logger.error(f"Error occured in create-todo: {str(e)}")
             return Response({
                 'status':0,
                 'message':str(e),
@@ -72,41 +79,46 @@ class Create_todo(APIView):
 class Delete_todo(APIView):
     ''' delete a todo with given slug '''
     def delete(self,request,*args,**kwargs):
-
-        todo_slug  = request.data.get('todo_slug')
-        if todo_slug:
-            todo = Todo_model.objects.filter(slug_field = todo_slug).first()
-            
-            if todo:
-                is_verified = is_user_owner(todo.created_by,request.user)
-                if is_verified:
-                    # todo.delete()
-                    return Response({
-                        'status':1,
-                        'message':"Todo deleted successfully",
-                        'data':None
-                    },status=status.HTTP_200_OK)
+        try:
+            todo_slug  = request.data.get('todo_slug')
+            if todo_slug:
+                todo = Todo_model.objects.filter(slug_field = todo_slug).first()
+                
+                if todo:
+                    is_verified = is_user_owner(todo.created_by,request.user)
+                    if is_verified:
+                        # todo.delete()
+                        return Response({
+                            'status':1,
+                            'message':"Todo deleted successfully",
+                            'data':None
+                        },status=status.HTTP_200_OK)
+                    else:
+                        return Response({
+                            'status':0,
+                            'message':"You are not authorized to perform this operation",
+                            'data':None
+                        },status=status.HTTP_401_UNAUTHORIZED)
                 else:
                     return Response({
                         'status':0,
-                        'message':"You are not authorized to perform this operation",
-                        'data':None
-                    },status=status.HTTP_401_UNAUTHORIZED)
+                        'message':"Todo does not exist",
+                        "data":None
+
+                    },status=status.HTTP_404_NOT_FOUND)
             else:
                 return Response({
                     'status':0,
-                    'message':"Todo does not exist",
-                    "data":None
-
+                    'message':"Please provide todo slug to be deleted",
+                    'data':None
                 },status=status.HTTP_404_NOT_FOUND)
-        else:
+        except Exception as e:
+            logger.error(f"Error occured in delete-todo: {str(e)}")
             return Response({
-                'status':0,
-                'message':"Please provide todo slug to be deleted",
-                'data':None
-            },status=status.HTTP_404_NOT_FOUND)
-    
-
+                    'status':0,
+                    'message':"Internal server error",
+                    'data':None
+                },status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class Update_todo(APIView):
     def put(self, request,*args,**kwargs):
@@ -128,6 +140,7 @@ class Update_todo(APIView):
                 'data': serializer.data
             }, status=status.HTTP_200_OK)
         else:
+            logger.error(f"Error occured in delete-todo: {serializer.errors}")
             return Response({
                 'status': 0,
                 'message': serializer.errors,
